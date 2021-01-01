99c3c8a92ef31a0281c313193c98bcda1d3a0f0a
NikMa <nikitama1hr@gmail.com>
2021-01-01 20:34:31 +0300

    remove static products.js add axios request on server add currently on package.json

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from "react-router-dom";
 import {Row, Col, Image, ListGroup, Button} from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
-const ProductScreen = ({match}) => {
-    const product = products.find(p => p._id === match.params.id)
+const ProductScreen = ({    match}) => {
+    const [product, setProduct] = useState({});
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [match]);
+
     return (
         <>
             <Link className='btn btn-dark my-3' to='/'> GO back</Link>
@@ -47,7 +55,7 @@ const ProductScreen = ({match}) => {
                                     Status:
                                 </Col>
                                 <Col>
-                                    <strong>${product.countInStock >= 0 ?'In stock':'Out of Stock'}</strong>
+                                    <strong>${product.countInStock >= 0 ? 'In stock' : 'Out of Stock'}</strong>
                                 </Col>
                             </Row>
                         </ListGroup.Item>
