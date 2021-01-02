d81e83c03c2bd9b7956ba9f5927b1c352af817e7
NikMa <nikitama1hr@gmail.com>
2021-01-02 08:48:52 +0300

    replacement require on import add env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const products = require('./data/products')
+import express from 'express';
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
+
 const app = express()
 app.get('/', (req, res) => {
     res.send('Api running')
@@ -8,8 +12,11 @@ app.get('/api/products', (req, res) => {
     res.json(products)
 })
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p)=>p._id === req.params.id)
+    const product = products.find((p) => p._id === req.params.id)
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running ^^'))
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on ${PORT} ^^`))
