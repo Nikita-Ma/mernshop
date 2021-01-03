b36b209126d624c55aac3fe131b5b78c6500c297
NikMa <nikitama1hr@gmail.com>
2021-01-03 15:33:58 +0300

    qty select and add functional cart

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+
 const App = () => {
     return (
         <Router>
             <Header/>
             <main className='py-3'>
                 <Container>
-                   <Route path='/' component={HomeScreen} exact/>
-                   <Route path='/product/:id' component={ProductScreen} exact/>
+                    <Route path='/' component={HomeScreen} exact/>
+                    <Route path='/product/:id' component={ProductScreen} exact/>
+                    <Route path='/cart/:id?' component={CartScreen} exact/>
                 </Container>
             </main>
             <Footer/>
