72d93082557627267a805be37dc88b60cbce00fe
NikMa <nikitama1hr@gmail.com>
2021-01-04 08:20:34 +0300

    use controllers and auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -18,8 +20,13 @@ const userSchema = mongoose.Schema({
         required: true,
         default: false
     }
-  }, {
+}, {
     timestamps: true
 })
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return (await bcrypt.compare(enteredPassword, this.password))
+}
+
 const User = mongoose.model('User', userSchema)
 export default User
